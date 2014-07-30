044321e8e6d2497914758316a9ecfee78af1f56d
guyun <guyun@itouzi.com>
2014-07-30 10:25:24 +0800

    世界杯活动

--- worldcup.sh
+++ worldcup.sh
@@ -1,15 +1,18 @@
 #!bin/bash
 source ../conf/config.sh
+dd=$1
+#echo -n "本次活动一共被邀请投资的人数和投资总额: "
 
-echo -n "本次活动一共被邀请投资的人数和投资总额: "
-echo "select count(distinct user_id),sum(account) from dw_borrow_tender where user_id in (select user_id from dw_user where addtime>=1402588800 and addtime<=1405267199 and invite_userid>0 and invite_userid not in ('27225','3582','27222','6729','380','27221','507','562','6570','7440','5437','30998','32123','41408','81589','83032','85113'))"|$MYSQL|awk '{if(NR>1)print}' 
+#echo "select count(user_id),sum(account) from dw_borrow_tender where user_id in (select invite_userid from dw_user where addtime<=1405267199 and addtime>=1402588800 and invite_userid>0 and invite_userid not in ('27225','3582','27222','6729','380','27221','507','562','6570','7440','5437','30998','32123','41408','81589','83032','85113')) and addtime<=1405267199 and addtime>=1402588800"|$MYSQL|awk '{if(NR>1)print}' 
 
-echo "select user_id,account,addtime from dw_borrow_tender where user_id in (select * from (select user_id from dw_borrow_tender where user_id in (select user_id from dw_user where addtime>=1402588800 and addtime<=1405267199 and invite_userid>0 and invite_userid not in ('27225','3582','27222','6729','380','27221','507','562','6570','7440','5437','30998','32123','41408','81589','83032','85113'))group by user_id having count(1)>=2) t )" |$MYSQL|awk '{if(NR>1)print}'|sort -k 1 -k 3 -n >first_invest.txt
+echo "select distinct invite_userid from dw_user where addtime<=1405267199 and addtime>=1402588800 and invite_userid>0 and invite_userid not in ('27225','3582','27222','6729','380','27221','507','562','6570','7440','5437','30998','32123','41408','81589','83032','85113')"|$MYSQL|awk '{if(NR>1)print}'>invite_userid.txt 
+echo "select user_id,account  from dw_borrow_tender where addtime<=1405267199 and addtime>=1402588800"|$MYSQL|awk '{if(NR>1)print}' >tender_userid.txt
 
-echo -n "本次活动有被邀请投资人中复投的人数:"
-echo "select count(distinct user_id) from dw_borrow_tender where user_id in (select * from (select user_id from dw_borrow_tender where user_id in (select
-user_id from dw_user where addtime>=1402588800 and addtime<=1405267199 and invite_userid>0 and invite_userid not in ('27225','3582','27222','6729','380','27221','507','562','6570','7440','5437','30998','32123','41408','81589','83032','85113'))group by user_id having count(1)>=2) t )" |$MYSQL|awk '{if(NR>1)print}'
-echo -n "复投周期,100-500,500-1K,1K-5K,5K-1W,1W以上):"
+#echo "select user_id,account,addtime from dw_borrow_tender where user_id in (select * from (select user_id from dw_borrow_tender where user_id in (select user_id from dw_user where addtime<=1405267199 and addtime>=1402588800 and invite_userid>0 and invite_userid not in ('27225','3582','27222','6729','380','27221','507','562','6570','7440','5437','30998','32123','41408','81589','83032','85113'))group by user_id having count(1)>=2) t )" |$MYSQL|awk '{if(NR>1)print}'|sort -k 1 -k 3 -n >first_invest.txt
+
+#echo -n "本次活动有被邀请投资人中复投的人数:"
+#echo "select count(distinct user_id) from dw_borrow_tender where user_id in (select * from (select user_id from dw_borrow_tender where user_id in (select user_id from dw_user where addtime<=1405267199 and addtime>=1402588800 and invite_userid>0 and invite_userid not in ('27225','3582','27222','6729','380','27221','507','562','6570','7440','5437','30998','32123','41408','81589','83032','85113'))group by user_id having count(1)>=2) t )" |$MYSQL|awk '{if(NR>1)print}'
+#echo -n "复投周期,100-500,500-1K,1K-5K,5K-1W,1W以上):"
 
 awk 'BEGIN{
     sum=0;
